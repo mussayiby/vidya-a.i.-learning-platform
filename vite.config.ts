18121346373cb5fc66ba60efaca817318e4068e7
mussayib <mussayibdros@gmail.com>
2026-08-30 22:29:48 +0530

    goopt

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,6 @@
-// @lovable.dev/vite-tanstack-config already includes the
-// TanStack Start, React, Tailwind, Nitro and other required plugins.
-
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import fs from "node:fs";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-  },
-
-  vite: {
-    server: {
-      host: "0.0.0.0",
-      port: 8080,
-      strictPort: true,
-
-      https: {
-        key: fs.readFileSync(
-          "cert/localhost-key.pem",
-        ),
-        cert: fs.readFileSync(
-          "cert/localhost.pem",
-        ),
-      },
-    },
-  },
+  plugins: [react()],
 });
